1c7efeea35201e79b51091cee3fdfa012ffeaf84
Lhuan Cassio <lhuandev@outlook.com>
2026-02-20 20:17:36 -0400

    reestruturacao backend

---
+++ backend/financas-service/src/main/java/com/app/hubpessoal/financas_service/FinancasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.hubpessoal.financas_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasServiceApplication.class, args);
+	}
+
+}
---
+++ backend/financas-service/src/test/java/com/app/hubpessoal/financas_service/FinancasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hubpessoal.financas_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
