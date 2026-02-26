b497a5d2d22ef8e24c692e6c37c5e3570a1a68cf
Lhuan Cassio <lhuandev@outlook.com>
2026-02-25 22:25:29 -0400

    Versionando mudanças

--- backend/financas-service/src/main/java/com/app/hubpessoal/financas_service/FinancasServiceApplication.java
+++ backend/financas-service/src/main/java/com/app/hubpessoal/financas_service/FinancasServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinancasServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FinancasServiceApplication.class, args);
 	}
-
 }
